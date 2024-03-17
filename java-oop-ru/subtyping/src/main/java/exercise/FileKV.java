5141ed6ff1e41eb9eb706704d64508e0e57f156f
@hexlet/cli <support@hexlet.io>
2024-03-17 11:17:09 +0600

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
+++ java-oop-ru/subtyping/src/main/java/exercise/FileKV.java
@@ -5,32 +5,37 @@ import java.util.Map;
 // BEGIN
 public class FileKV implements KeyValueStorage {
 
-    public FileKV(String path, Map<String, String> start) {
-        this.path = path;
-        this.start = start;
+    private String filepath;
+
+    FileKV(String filepath, Map<String, String> initial) {
+        this.filepath = filepath;
+        initial.entrySet().stream().forEach(entry -> set(entry.getKey(), entry.getValue()));
     }
 
-    private String path;
-    private Map<String, String> start;
-
-    @Override
     public void set(String key, String value) {
-
+        String content = Utils.readFile(filepath);
+        Map<String, String> data = Utils.unserialize(content);
+        data.put(key, value);
+        Utils.writeFile(filepath, Utils.serialize(data));
     }
 
-    @Override
     public void unset(String key) {
-
+        String content = Utils.readFile(filepath);
+        Map<String, String> data = Utils.unserialize(content);
+        data.remove(key);
+        Utils.writeFile(filepath, Utils.serialize(data));
     }
 
-    @Override
     public String get(String key, String defaultValue) {
-        return null;
+        String content = Utils.readFile(filepath);
+        Map<String, String> data = Utils.unserialize(content);
+        return data.containsKey(key) ? data.get(key) : defaultValue;
     }
 
-    @Override
     public Map<String, String> toMap() {
-        return null;
+        String content = Utils.readFile(filepath);
+        Map<String, String> data = Utils.unserialize(content);
+        return data;
     }
 }
 // END
